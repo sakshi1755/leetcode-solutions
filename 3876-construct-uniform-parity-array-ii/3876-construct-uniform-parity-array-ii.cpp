ab663e9f10bf16ff6de0eadb932da15ed39f8bef
sakshi1755 <153982110+sakshi1755@users.noreply.github.com>
2026-03-24 18:20:17 +0530

    Time: 4 ms (67.34%), Space: 165.8 MB (66.22%) - LeetHub

---
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        bool isodd=false;
+        int n=nums1.size();
+        bool iseven=false;
+        for(int i=0;i<n;i++){
+            if(nums1[i]%2==0)iseven=true;
+            else isodd=true;
+
+        }
+        if(isodd^iseven)return true;
+        int mini=*min_element(nums1.begin(),nums1.end());
+        if(mini%2==0)return false;
+        else return true;
+    }
+};
