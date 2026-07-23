2cdfc220446bf3b5f1e828aa3af530917a8c07b9
sakshi1755 <153982110+sakshi1755@users.noreply.github.com>
2026-07-24 00:00:23 +0530

    Time: 1428 ms (26.58%), Space: 0B (100%) - LeetHub

---
+++ 1158-market-analysis-i/1158-market-analysis-i.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select u.user_id as buyer_id , u.join_date, coalesce(t.orders_in_2019,0) as orders_in_2019 from Users u left join (select buyer_id, count(*) as orders_in_2019 from Orders where order_date>='2019-01-01' and order_date<='2019-12-31' group by buyer_id)t on u.user_id=t.buyer_id
